deb860132c88490ba381e6ab01a6d71f25c5a36f
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2023-08-22 08:43:00 +0200

    Clear repo

--- Pods/Target Support Files/Pods-sample-apps-ios-workspace-swiftUITests/Pods-sample-apps-ios-workspace-swiftUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_sample_apps_ios_workspace_swiftUITests : NSObject
-@end
-@implementation PodsDummy_Pods_sample_apps_ios_workspace_swiftUITests
-@end
